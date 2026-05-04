0eba56f48303974db49e656c32f96a7a4ee398dc
Lakshay <black.leopard0987@gmail.com>
2026-05-05 01:47:42 +0530

    Files Renamed

--- 000_HelloWorld.py
+++
@@ -1,9 +0,0 @@
-# Input
-name=input("Enter Your Character Name:\n")
-
-# Output
-# (i) print() => It shows text, numbers, or results of code so you can see them
-print("\nHello World")
-
-# f-string (formatted string literals) => f-strings (formatted string literals) let you embed variables directly inside a string
-print(f"Character Name is {name} \nHave a great day ")
