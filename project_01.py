bbd3c810445ccb325ab62df235c8c64001d5d43c
Lakshay <black.leopard0987@gmail.com>
2026-05-10 05:14:39 +0530

    Rock, Paper, Scissors, Lizard and Spock Game

--- project_01.py
+++ project_01.py
@@ -1,33 +1,75 @@
-# Snake Water Gun Game
 import random
-''' 1 for snake, -1 for water, 0 for gun '''
-computer = random.choice([-1, 0, 1])
-youstr = input("Enter your choice from(s for snake, w for water, g for gun): ")
-youDict = {"s": 1, "w": -1, "g": 0}
-reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
-you = youDict[youstr]
 
-# By now we have 2 numbers (variables), you and computer
-print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
+# Rock, Paper, Scissors, Lizard, Spock Game
+# 0 = Rock, Defeats = Scissors, Lizard
+# 1 = Paper, Defeats = Rock, Spock
+# 2 = Scissors, Defeats = Paper, Lizard
+# 3 = Lizard, Defeats = Spock, Paper
+# 4 = Spock, Defeats = Scissors, Rock
+moves = {0: "Rock", 1: "Paper", 2: "Scissors", 3: "Lizard", 4: "Spock"}
+winning_rules = [[2, 3], [0, 4], [3, 1], [1, 4], [0, 2]]
 
-if computer == you:
-    print("Its a draw")
-else:
-     if(computer ==-1 and you == 1): 
-        print("You win!")
-     elif(computer ==-1 and you == 0): 
-        print("You Lose!")
-     elif(computer == 1 and you == -1): 
-        print("You lose!")
-     elif(computer ==1 and you == 0): 
-        print("You Win!")
-     elif(computer ==0 and you == -1): 
-        print("You Win!")
-     elif(computer == 0 and you == 1): 
-        print("You Lose!") 
 
-# The below logic is written on the basis of the value of computer - you 
-# if((computer - you) == -1 or  (computer - you) == 2 ):
-#     print("You lose!")                                    
-# else:
-#     print("You win!") 
+def show_rules():
+    print("Valid Moves : ")
+    for move in moves:
+        print(f"{move} -> {moves.get(move)}")
+    print()
+
+
+def get_user_move():
+    is_valid_move = False
+    user_move = ""
+    while not is_valid_move:
+        user_move = input("Play your move : ")
+        is_valid_move = user_move.isnumeric() and int(user_move) in moves
+        if not is_valid_move:
+            print("Play valid move.")
+            show_rules()
+    return int(user_move)
+
+
+def show_result(user_move, computer_move):
+    user_result = computer_move in winning_rules[user_move]
+    message = ""
+    if user_move == computer_move:
+        message = f"Match DRAW, You and Computer both chooses {moves.get(user_move)}"
+    elif user_result:
+        message = f"You WIN, {moves.get(user_move)} DEFEATS {moves.get(computer_move)}."
+    else:
+        message = (
+            f"Computer WIN, {moves.get(computer_move)} DEFEATS {moves.get(user_move)}."
+        )
+    print(message)
+
+
+def main():
+    try:
+        print("Rock, Paper, Scissors, Lizard and Spock")
+        show_rules()
+
+        playing = True
+
+        while playing:
+            computer_move = random.randint(0, len(moves) - 1)
+            user_move = get_user_move()
+
+            print()
+            print(f"Your move -> {moves.get(user_move)}")
+            print(f"Computer move -> {moves.get(computer_move)}")
+            print()
+
+            show_result(user_move=user_move, computer_move=computer_move)
+            print("\n" * 2)
+
+            playing = (
+                True if input("Continue playing (y/n)? : ").lower() == "y" else False
+            )
+
+    except KeyboardInterrupt:
+        print()
+        print("Game Interrupted.")
+
+
+if __name__ == "__main__":
+    main()
