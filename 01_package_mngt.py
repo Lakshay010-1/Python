f47c2cac7009a8711146b4ed26b156240c1ed740
Lakshay <black.leopard0987@gmail.com>
2026-05-16 22:48:07 +0530

    Package Management

---
+++ 01_package_mngt.py
@@ -0,0 +1,68 @@
+# In terminal, Python uses REPL(Read Evaluate Print Loop).
+# Module  - Module is a single file(.py) containing code written by somebody else (usually) which can be imported and used in our programs.
+# Package - A package is a directory containing multiple related modules.
+# PyPI    - Python Package Index (PyPI) is a repository of packages for the python.
+# PIP     - Pip is the package manager for python. You can use pip to install/uninstall packages on your system.
+#           Command : python -m pip <install / uninstall> -U <package_name>
+#                     ~ python -m pip = run pip module using THIS python interpreter
+#                     ~ -m            = run library module as script
+#                     ~ -U            = updates package to latest version (--upgrade)
+
+# Usuage
+# Approach 1) import <package_name> OR import <package_name> as alias_name
+# Approach 2) from <package_name> import <class/function>
+# Approach 3) from <package_name> import *
+
+# os Package
+from os import listdir
+
+# Specify the Directory you want to list
+directory_path = "/"
+
+# list all files and directories in the specified path
+contents = listdir(directory_path)
+
+# Print each file and directory name
+for item in contents:
+    print(item)
+
+
+# 'pyttsx3' Package
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("I Can Do this all Day...")
+engine.runAndWait()
+
+# Changing Voice , Rate and Volume
+""" 
+import pyttsx3
+engine = pyttsx3.init()                                             # object creation
+
+# RATE
+rate = engine.getProperty('rate')                                   # getting details of current speaking rate
+print (rate)                                                        # printing current voice rate
+engine.setProperty('rate', 125)                                     # setting up new voice rate
+
+
+# VOLUME
+volume = engine.getProperty('volume')                               # getting to know current volume level (min=0 and max=1)
+print (volume)                                                      # printing current volume level
+engine.setProperty('volume',1.0)                                    # setting up volume level  between 0 and 1
+
+# VOICE
+voices = engine.getProperty('voices')                               # getting details of current voice
+#engine.setProperty('voice', voices[0].id)                          # changing index, changes voices. o for male
+engine.setProperty('voice', voices[1].id)                           # changing index, changes voices. 1 for female
+
+engine.say("Hello World!")
+engine.say('My current speaking rate is ' + str(rate))
+engine.runAndWait()
+engine.stop()
+
+# Saving Voice to a file
+# On linux make sure that 'espeak' and 'ffmpeg' are installed
+engine.save_to_file('Hello World', 'test.mp3')
+engine.runAndWait()
+
+"""
